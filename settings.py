8ef2d60c40b2e3ec8f5b562713972538450d0748
Mo Assaf <mormoassaf@gmail.com>
2023-05-11 13:57:55 +0200

    classifier works!

---
+++ settings.py
@@ -0,0 +1,96 @@
+categories = [
+    {
+        "name": "universalism",
+        "description": "Understanding, appreciation, tolerance, and protection for the welfare of all people and for nature.",
+        "examples": [
+            "Equality is important to me.",
+            "It is good to accept and try to understand those who are different from oneself."
+        ],
+        "children": []
+    },
+    {
+        "name": "self-direction",
+        "description": "Independent thought and action; choosing, creating, exploring.",
+        "examples": [
+            "It is good to search for the truth and think in a rational and unbiased way.",
+            "Independence is important for me to make my own decisions.",
+        ],
+        "children": []
+    },
+    {
+        "name": "stimulation",
+        "description": "It is good to experience excitement, novelty, and change.",
+        "examples": [
+            "Have an exciting life: arguments towards allowing people to experience foreign places and special activities or having perspective-changing experiences",
+            "Have a varied life: arguments towards allowing people to engage in many activities and change parts of their life or towards promoting local clubs (sports, ...)",
+            "Be daring: arguments towards more risk-taking"
+        ],
+        "children": []
+    },
+    {
+        "name": "hedonism",
+        "description": "Pleasure or sensuous gratification for oneself.",
+        "examples": [
+            "It is important to do things that make you feel good.",
+            "It is important to have a good time.",
+        ],
+        "children": []
+    },
+    {
+        "name": "achievement",
+        "description": "It is good to be successful in accordance with social norms.",
+        "examples": [
+            "It is important to show your abilities.",
+            "Be ambitious: arguments towards allowing for ambitions and climbing up the social ladder",
+            "Have success: arguments towards allowing for success and recognizing achievements",
+            "Be capable: arguments towards acquiring competence in certain tasks, being more effective, and showing competence in solving tasks",
+            "Be intellectual: arguments towards acquiring high cognitive skills, being more reflective, and showing intelligence",
+            "Be courageous: arguments towards being more courageous and having people stand up for their beliefs"
+        ],
+        "children": []
+    },
+    {
+        "name": "power",
+        "description": "Social status and prestige, control or dominance over people and resources.",
+        "examples": [
+            "It is important to be rich.",
+            "It is good to be in positions of control over others.",
+        ],
+        "children": []
+    },
+    {
+        "name": "security",
+        "description": "Safety, harmony, and stability of society, of relationships, and of self.",
+        "examples": [
+            "It is important to live in secure surroundings.",
+            "It is important to plan ahead so as to avoid surprises.",
+        ],
+        "children": []
+    },
+    {
+        "name": "tradition",
+        "description": "Respect, commitment, and acceptance of the customs and ideas that traditional culture or religion provide the self.",
+        "examples": [
+            "It is important to uphold traditions.",
+            "It is important to follow the customs of my religion.",
+        ],
+    },
+    {
+        "name": "conformity",
+        "description": "Restraint of actions, inclinations, and impulses likely to upset or harm others and violate social expectations or norms.",
+        "examples": [
+            "It is important to behave properly.",
+            "It is important to respect traditions.",
+        ],
+        "children": []
+    },
+    {
+        "name": "benevolence",
+        "description": "Preservation and enhancement of the welfare of people with whom one is in frequent personal contact.",
+        "examples": [
+            "It is important to help the people around you.",
+            "It is important to be loyal to your friends.",
+        ],
+        "children": []
+    }
+]
